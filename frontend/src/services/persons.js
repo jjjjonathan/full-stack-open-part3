bbd6b87f5e727aea6eae108647a888672c85c10d
jjjjonathan <hornathanjon@gmail.com>
2021-03-30 01:14:36 -0500

    exercise 3.11 complete

---
+++ frontend/src/services/persons.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const url = "/api/persons";
+
+const getAll = () => {
+  return axios.get(url);
+};
+
+const create = (newObject) => {
+  return axios.post(url, newObject);
+};
+
+const deleteEntry = (id) => {
+  return axios.delete(`${url}/${id}`);
+};
+
+const put = (objectToUpdate) => {
+  return axios.put(`${url}/${objectToUpdate.id}`, objectToUpdate);
+};
+
+// eslint-disable-next-line
+export default { getAll, create, deleteEntry, put };
