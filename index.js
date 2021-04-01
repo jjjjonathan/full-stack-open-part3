8174ac27532142b7292bdf5d90836ec9a022dd6d
jjjjonathan <hornathanjon@gmail.com>
2021-03-31 23:02:15 -0500

    exercise 3.15 complete

--- index.js
+++ index.js
@@ -35,13 +35,15 @@ app.get('/api/persons/:id', (request, response) => {
     });
 });
 
-/* app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 });
-*/
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
